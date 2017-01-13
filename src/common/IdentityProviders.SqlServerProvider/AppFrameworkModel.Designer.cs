fae9e69a90684fd653f28c9dd15cb61d9e3d3b88
ktabarez <kaintabarez@gmail.com>
2017-01-12 21:04:29 -0800

    Fixed 500 error. Some projects were targeting .net 4.5.1 and 4.5 also nuget packages. Made all target 4.5. Also renamed web solution folder to web.auth. Renamed namepsaces to web.auth.

--- src/common/IdentityProviders.SqlServerProvider/AppFrameworkModel.Designer.cs
+++ src/common/IdentityProviders.SqlServerProvider/AppFrameworkModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\SourceControl\Git\common\IdentityProviders.SqlServerProvider\AppFrameworkModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\repositories\github\apps\src\common\IdentityProviders.SqlServerProvider\AppFrameworkModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
